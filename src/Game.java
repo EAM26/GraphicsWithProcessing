9332f0773bd88b2e914712ea8031aa3940883889
EAM26 <emuller2607@gmail.com>
2024-03-30 13:48:26 +0100

    Setup Papplet with core.jar lib

---
+++ src/Game.java
@@ -0,0 +1,21 @@
+import processing.core.PApplet;
+
+public class Game extends PApplet {
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+
+
+    public static void main(String[] args) {
+        PApplet.main("Game");
+    }
+}
